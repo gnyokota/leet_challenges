36d027986d2bdd009aee24d675a67cb7c266ef66
giovanayokota <gnyokota@gmail.com>
2024-01-19 17:30:21 +0100

    add add ScopeFunction

---
+++ src/main/kotlin/com/leet/code/ScopeFunction.kt
@@ -0,0 +1,32 @@
+package com.leet.code
+
+data class Student(var firstName: String, var lastName: String)
+
+fun main() {
+    val str = "hello"
+    //this scope
+    str.run {
+        println("The string has length $length")
+    }
+
+    //it scope
+    str.let {
+        println("The string has length ${it.length}")
+    }
+    val numbers = mutableListOf(1, 2, 3, 4, 5)
+    numbers.map { it * 2 }.filter { it > 5 }.let { println(it) }
+
+    //this scope
+    val student = Student("John", "Doe")
+    student.apply {
+        firstName = "Ann"
+    }
+    println("Student: $student")
+
+    val numbersEven = mutableListOf(2, 4, 6,8)
+    with(numbers){
+        println("The lengh of the list is $size")
+        val sum = numbers.sum()
+        println("The sum of the list is: $sum")
+    }
+}
