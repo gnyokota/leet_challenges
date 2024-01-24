1cf927f378c511cc3dfcbfe726d21c7fce739afe
giovanayokota <gnyokota@gmail.com>
2024-01-24 22:16:48 +0100

    add pseudoPalindromic

--- src/main/kotlin/com/leet/code/FindErrorNums.kt
+++ src/main/kotlin/com/leet/code/FindErrorNums.kt
@@ -2,10 +2,10 @@ package com.leet.code
 
 class FindErrorNums {
     fun findErrorNums(nums: IntArray): IntArray {
-        val size = nums.size //4
-        var sum = size * (size + 1) / 2 //10
+        val size = nums.size
+        var sum = size * (size + 1) / 2
         val result = IntArray(2)
-        val seen = BooleanArray(size + 1) //5
+        val seen = BooleanArray(size + 1)
         for (num in nums) {
             sum -= num
             if (seen[num]) result[0] = num
